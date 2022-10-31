2b9bf80af16ffeb1d38b00e3a0051b3aaccd38a4
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-10-31 07:17:55 +0000

    transition dipole moment

--- cis.jl
+++ cis.jl
@@ -1,4 +1,5 @@
 using PyCall
+using LinearAlgebra
 pyscf = pyimport("pyscf")
 geomopt = pyimport("pyscf.geomopt.geometric_solver")
 
@@ -20,4 +21,60 @@ function construct_cis_matrix(mol, hf)
     end
 
     reshape(cis, (nocc * nvir, nocc * nvir))
+end
+
+function construct_cis_matrix_smart(mol, hf)
+    nao = py"int"(mol.nao)
+    nocc = mol.nelectron ÷ 2
+    nvir = nao - nocc
+
+    occ = 1:nocc
+    vir = (nocc+1):nao
+
+    c_occ = hf.mo_coeff[:, occ]
+    c_vir = hf.mo_coeff[:, vir]
+
+    g_ao = mol.intor("int2e", aosym="s8")
+    g_col = reshape(
+        pyscf.ao2mo.incore.general(g_ao, (c_vir, c_vir, c_occ, c_occ),
+            compact=false),
+        (nvir, nvir, nocc, nocc)
+    )
+    g_exc = reshape(
+        pyscf.ao2mo.incore.general(g_ao, (c_occ, c_vir, c_vir, c_occ),
+            compact=false),
+        (nvir, nocc, nocc, nvir)
+    )
+
+    cis = [2g_exc[a, i, j, b] - g_col[a, b, j, i]
+           for i in occ, a in 1:nvir, j in occ, b in 1:nvir]
+
+    for i in occ, (na, a) in enumerate(vir)
+        cis[i, na, i, na] += hf.mo_energy[a] - hf.mo_energy[i]
+    end
+
+    Symmetric(reshape(cis, (nocc * nvir, nocc * nvir)))
+end
+
+function transition_dipole(mol, hf, cis_vec, proj_norm=true)
+    nao = mol.nao
+    nocc = mol.nelectron ÷ 2
+    nvir = nao - nocc
+
+    occ = 1:nocc
+    vir = (nocc+1):nao
+
+    c_occ = hf.mo_coeff[:, occ]
+    c_vir = hf.mo_coeff[:, vir]
+
+    d_ov = reshape(pyscf.lib.einsum("qxy,xi,yj->qij",
+            mol.intor("int1e_r"), c_occ, c_vir), (3, nocc * nvir))
+    
+    td = pyscf.lib.einsum("qi,ij->qj", d_ov, cis_vec)
+
+    if proj_norm
+        [norm(r) for r in eachcol(td)]
+    else
+        td
+    end
 end
--- example.jl
+++ example.jl
@@ -6,22 +6,23 @@ const au2ev = 27.21138624598853
 
 # Comparing CIS, CCSD and TDHF excitation energies
 function h2o()
-    mol = pyscf.M(atom="O 0 0 0; H 1 0 0; H 0 1 0"; basis="ccpvdz", verbose=0)
-    mol = geomopt.optimize(mol.RHF())
+    mol = pyscf.M(atom="O 0 0 0; H 1 0 0; H 0 1 0"; basis="ccpvdz")
+    # mol = geomopt.optimize(mol.RHF())
 
     hf = mol.RHF().run()
 
-    cis = @time construct_cis_matrix(mol, hf)
+    cis = @time construct_cis_matrix_smart(mol, hf)
 
     n_ex = 3
 
-    cis_ee, cis_c = eigen(cis)
+    cis_ee = eigvals(cis, 1:n_ex)
 
-    cis_ee = cis_ee[1:n_ex] * au2ev
+    cis_ee = cis_ee * au2ev
 
     tdhf = hf.TDHF()
     tdhf.nstates = n_ex
     tdhf.run()
+    tdhf.analyze()
     tdhf_ee = tdhf.e * au2ev
 
     ccsd_ee = hf.CCSD().run().eomee_ccsd_singlet(nroots=n_ex)[1] * au2ev
@@ -37,4 +38,60 @@ function h2o()
             rpad(tdhf, 20), "    ",
             rpad(ccsd, 20))
     end
+end
+
+function h2o_transition()
+    mol = pyscf.M(atom="O 0 0 0; H 1 0 0; H 0 1 0"; basis="ccpvdz")
+    # mol = geomopt.optimize(mol.RHF())
+
+    hf = mol.RHF().run()
+
+    cis = @time construct_cis_matrix_smart(mol, hf)
+
+    n_ex = 3
+
+    cis_ee, cis_v = eigen(cis, 1:n_ex)
+
+    cis_ee = cis_ee * au2ev
+    @show cis_ee
+
+    tdhf = hf.TDHF()
+    tdhf.nstates = n_ex
+    tdhf.run()
+    tdhf.analyze()
+
+    transition_dipole(mol, hf, cis_v)
+end
+
+function benzene()
+    mol = pyscf.M(atom="""
+    C  -1.664244  -0.360809   0.000000
+    C  -0.275880  -0.334582   0.000000
+    C   0.395745   0.880796   0.000000
+    C  -0.321200   2.070000   0.000000
+    C  -1.709559   2.043702  -0.000000
+    C  -2.381247   0.828355  -0.000000
+    H  -3.463284   0.807970  -0.000000
+    H  -2.187521  -1.308116   0.000000
+    H   0.282794  -1.261455   0.000000
+    H   1.477780   0.901181   0.000000
+    H   0.202080   3.017307   0.000000
+    H  -2.268234   2.970572  -0.000000
+    """; basis="ccpvdz")
+
+    hf = @time mol.RHF().run()
+
+    cis = @time construct_cis_matrix_smart(mol, hf)
+
+    n_ex = 6
+
+    cis_ee, cis_v = @time eigen(cis, 1:n_ex)
+
+    cis_ee = cis_ee * au2ev
+    @show cis_ee
+
+    nm = 1239.84193 ./ cis_ee
+    @show nm
+
+    @time transition_dipole(mol, hf, cis_v)
 end
